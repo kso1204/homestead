ee7d881040b9ec7d914459439e446f501292f543
vagrant <vagrant@homestead>
2020-07-20 08:57:59 +0000

    사용자 인증

--- code/Project1/routes/web.php
+++ code/Project1/routes/web.php
@@ -16,6 +16,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','WelcomeController@index');
 
+/*
+
+Route::get('auth/login',function(){
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password'
+    ];
+
+    if(!auth()->attempt($credentials)){
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+
+    return redirect('protected');
+});
+Route::get('protected', function(){
+    dump(session()->all());
+
+    if(!auth()->check()){
+        return ' 누구세요?';
+    }
+
+    return '어ㅏ서오세요'.auth()->user()->name;
+});
+
+Route::get('protected',['middleware'=>'auth',function(){
+
+}]);
+
+Route::get('auth/logout',function(){
+    auth()->logout();
+    return '또봐요';
+});
+*/
 Route::resource('articles','ArticlesController');
 /*
 Route::get('/',function(){
@@ -23,3 +56,6 @@ Route::get('/',function(){
     return view('welcome',['items'=>$items]
     );
 });*/
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
