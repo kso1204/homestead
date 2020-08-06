73d31c9c75362e5d810cb0ed5d869f35293111e7
vagrant <vagrant@homestead>
2020-08-06 08:55:27 +0000

    larave+vue example

--- code/mice_vue/resources/js/app.js
+++ code/mice_vue/resources/js/app.js
@@ -1,1 +1,53 @@
-require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//import VueRouter는 
+//vue-router.vue에서 선언한 var VueRouter의 내용을 가져온다고 생각
+// VueRouter<-- component에 들어가는 내용
+//https://kr.vuejs.org/v2/guide/components-registration.html
+
+Vue.use(VueRouter)
+
+import App from './views/App'
+import Hello from './views/Hello'
+import Home from './views/Home'
+import UsersIndex from './views/UsersIndex'
+import UsersEdit from './views/UsersEdit'
+import UsersCreate from './views/UsersCreate'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component : Hello
+        },
+        {
+            path: '/users',
+            name: 'users.index',
+            component: UsersIndex,
+        },
+        {
+            path: '/users/:id/edit',
+            name: 'users.edit',
+            component: UsersEdit,
+        },
+        {
+            path: '/users/create',
+            name: 'users.create',
+            component: UsersCreate,
+        },
+    ],
+});
+
+const app = new Vue({
+    el :'#app',
+    components: { App },
+    router,
+});
--- code/mice_vue/routes/api.php
+++ code/mice_vue/routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/*
+Route::get('/users', function(){
+    if (rand(1, 10) < 5) {
+        abort(500, 'We could not retrieve the users');
+    }
+
+    return factory('App\User', 10)->make();
+});*/
+
+Route::namespace('Api')->group(function(){
+    Route::get('/users', 'UsersController@index');
+    Route::get('/users/{user}', 'UsersController@show');
+    Route::put('/users/{user}', 'UsersController@update');
+    Route::delete('/users/{user}', 'UsersController@destroy');
+    Route::post('/users', 'UsersController@store');
+});
